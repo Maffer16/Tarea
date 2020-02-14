2afaf541bb910fab1d969ddb5eca04c6fd1a79b2
Maffer16 <maffermendoza@MacBook-Pro-de-Mafer-2.local>
2020-02-13 20:01:48 -0600

    Script 2

--- Script1.py
+++ Script1.py
@@ -1,5 +1,7 @@
 cont = 0
-for n in range(0, 1000):
+inicio = int (input("Número inicial:"))
+fiin = int (input("Número final:"))
+for n in range(inicio, final):
     for d in range(1, n + 1):
       if n % d == 0:
         cont += 1
